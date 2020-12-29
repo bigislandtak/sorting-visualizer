f3ae948d30c879fffa26f82556e1fca2b5183711
Takeyuki Oshima <bigislandtak>
2020-12-28 22:27:40 -0700

    Updates

--- precache-manifest.7fdc98de46bbd0c4fe7874511aec94e1.js
+++ precache-manifest.7fdc98de46bbd0c4fe7874511aec94e1.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7dfb84da011ef12648fcd44bd357f2b4",
+    "revision": "8391a8d006cf76deca71e9c96e0bc586",
     "url": "/sorting-visualizer/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/sorting-visualizer/static/css/2.2a86faee.chunk.css"
   },
   {
-    "revision": "099240dc49660aac5152",
+    "revision": "5a7a5c7a2886a8ad5da8",
     "url": "/sorting-visualizer/static/css/main.c45a3be7.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/sorting-visualizer/static/js/2.09bd454b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "099240dc49660aac5152",
-    "url": "/sorting-visualizer/static/js/main.90bffbf2.chunk.js"
+    "revision": "5a7a5c7a2886a8ad5da8",
+    "url": "/sorting-visualizer/static/js/main.4b018a0b.chunk.js"
   },
   {
     "revision": "0b849e312d4bb7a17285",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sorting-visualizer/precache-manifest.5d36328b5121fd603b4b02c617b340ed.js"
+  "/sorting-visualizer/precache-manifest.7fdc98de46bbd0c4fe7874511aec94e1.js"
 );
 
 self.addEventListener('message', (event) => {
